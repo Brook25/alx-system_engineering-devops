0a35ed4145a6352f5790836c243800c6efe6590f
Brook25 <bekelebrook24@gmail.com>
2022-11-04 14:21:44 +0000

    Edits

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # This script creates a file in the /tmp directory
 file { '/tmp/school':
-  ensure  => 'file',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }

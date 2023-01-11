da33ff85de8642b4d157fe7d840bd6ece1e2e0df
Brook25 <bekelebrook24@gmail.com>
2023-01-11 11:06:13 +0000

    Edits

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """this script returns to-do list information for a given employee ID."""
-import requests, sys
+import requests
+import sys
 
 
 if __name__ == "__main__":
@@ -10,9 +11,8 @@ if __name__ == "__main__":
     name = requests.get(url + '/users/{}'.format(sys.argv[1])).json()['name']
 
     res = requests.get(url + '/todos', params={'userId': sys.argv[1]}).json()
-
-    cpd_tasks = [x for x in res if x['completed'] is True]
-    print('Employee {} is done with tasks({}/{}):'.format(name, len(cpd_tasks), len(res)))
-    for i in cpd_tasks:
-        print('\t' + i['title'])
-
+    tasks = [x for x in res if x['completed'] is True]
+    print('Employee {} is done with tasks({}/{}):'.
+          format(name, len(tasks), len(res)))
+    for i in tasks:
+        print('\t ' + i['title'])

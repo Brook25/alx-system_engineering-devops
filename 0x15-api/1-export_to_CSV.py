9aba3d3de1ec6bb35bd13ce2c1d30e57c85daf8a
Brook25 <bekelebrook24@gmail.com>
2023-01-12 11:16:51 +0000

    Edits

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""script exports to-do list information for a given employee ID to CSV format."""
-
+"""script exports to-do list info for a given employee ID to CSV format."""
 import csv
 import json
 import requests
 import sys
 
-url = 'https://jsonplaceholder.typicode.com/'
-user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
 
-res = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+    res = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
 
-fname = str(user['id']) + '.csv'
-with open(fname, 'x') as f:
-    fil_e = csv.writer(f, quoting=csv.QUOTE_ALL)
-    for row in res:
-        entry = [
-            row['userId'], user['username'], row['completed'], row['title']]
-        fil_e.writerow(entry)
+    fname = str(user['id']) + '.csv'
+    with open(fname, 'x') as f:
+        fil_e = csv.writer(f, quoting=csv.QUOTE_ALL)
+        for r in res:
+            entry = [r['userId'], user['username'], r['completed'], r['title']]
+            fil_e.writerow(entry)

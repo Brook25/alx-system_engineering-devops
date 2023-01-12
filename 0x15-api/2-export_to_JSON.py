76faea8f80612fa9b9fbed3126e683ac43816e6f
Brook25 <bekelebrook24@gmail.com>
2023-01-12 13:10:50 +0000

    Edits

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
 """script exports to-do list info for a given employee ID to JSON format."""
+
 import json
 import requests
 import sys
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.json".format(user_id), "w") as jsonfile:
-        json.dump({user_id: [{
-                "task": t.get("title"),
-                "completed": t.get("completed"),
-                "username": username
-            } for t in todos]}, jsonfile)
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
+    res = requests.get(url + 'todos', params={'userId': sys.argv[1]}).json()
+
+    for x in res:
+        x.pop('userId')
+        x.pop('id')
+        x['username'] = user['username']
+        x['task'] = x['title']
+        x.pop('title')
+
+
+    res = {user['id']: res}
+    fname = str(user['id']) + '.json'
+    with open(fname, 'w') as f:
+        json.dump(res, f)
